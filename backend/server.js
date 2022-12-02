d3e244e49d4284bf1ddd42971ccfa4471cc8188d
Doga Ister <dogaister@Dogas-MacBook-Pro-2.local>
2022-12-01 19:39:29 -0500

    Created frontend and backend folders, created a node server for API call

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const app = express();
+app.use(express.json());
+
+let response = null;
+let PORT = '4000';
+new Promise(async (resolve, reject) => {
+    try {
+        response = await axios.get(process.env.URL, {
+            headers: {
+                'X-CMC_PRO_API_KEY': process.env.API_KEY,
+                'Accept-Encoding': 'null',
+            },
+        });
+    } catch (ex) {
+        response = null;
+        // error
+        console.log(ex);
+        reject(ex);
+    }
+    if (response) {
+        // success
+        const json = response.data;
+        console.log(json);
+        resolve(json);
+    }
+});
+
+app.get('/api', (req, res) => {
+    res.send(response.data);
+});
+
+app.listen(PORT, () => {
+    console.log(`Express server is running on ${PORT}`);
+});
+
