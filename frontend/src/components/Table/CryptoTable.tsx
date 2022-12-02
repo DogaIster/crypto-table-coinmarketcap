55093789ae2c898b3bdd954778b4a95322ac84cf
Doga Ister <dogaister@Dogas-MacBook-Pro-2.local>
2022-12-02 16:19:10 -0500

    Used % svg from tabler icons, added id into interface to use it as an unique key prop

--- frontend/src/components/Table/CryptoTable.tsx
+++ frontend/src/components/Table/CryptoTable.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import {ICrypto} from "../../interfaces/ICrypto";
 import {Progress, Table} from "@mantine/core";
+import percentage from "../../assets/percentage.svg";
 
 function CryptoTable() {
     const [data, setData] = useState<any[]>([]);
@@ -17,16 +18,19 @@ function CryptoTable() {
 
     const rows = data["data" as unknown as number]?.map((value: ICrypto) => {
         const quoteUsdProperties = value.quote['USD']
-        const marketCapProgressBar = <Progress size="xl" radius="md" value={quoteUsdProperties.market_cap_dominance} />
+        const marketCapProgressBar = <Progress size="xl" radius="md" value={quoteUsdProperties.market_cap_dominance}/>
 
         return (
-            <tr>
+            <tr key={value.id}>
                 <td>{value.name}</td>
                 <td>{value.symbol}</td>
                 <td>{value.slug}</td>
                 <td>{(quoteUsdProperties.price).toFixed(2)}</td>
                 <td>{(quoteUsdProperties.percent_change_24h).toFixed(2)}</td>
-                <td>{marketCapProgressBar} {(quoteUsdProperties.market_cap_dominance).toFixed(2) + '%'}</td>
+                <td>{marketCapProgressBar} {(quoteUsdProperties.market_cap_dominance).toFixed(2)} <img width='15'
+                                                                                                       height='12'
+                                                                                                       src={percentage}/>
+                </td>
             </tr>
         )
     })
@@ -40,12 +44,12 @@ function CryptoTable() {
                     <th>Symbol</th>
                     <th>Slug</th>
                     <th>Price ($USD)</th>
-                    <th>Trading Volume(24h) %</th>
-                    <th>Market Cap %</th>
+                    <th>Trading Volume(24h) <img width='15' height='12' src={percentage}/></th>
+                    <th>Market Cap <img width='15' height='12' src={percentage}/></th>
                 </tr>
                 </thead>
                 <tbody>
-                    {rows}
+                {rows}
                 </tbody>
             </Table>
         </div>
--- frontend/src/interfaces/ICrypto.ts
+++ frontend/src/interfaces/ICrypto.ts
@@ -1,4 +1,5 @@
 export interface ICrypto {
+    id: number;
     quote: any;
     name: string,
     symbol: string,
